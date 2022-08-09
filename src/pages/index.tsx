cbe7c6672f5e39146d488fca5628e3b57f3bcbac
Theal Altake <zufa.altake@gmail.com>
2022-08-09 23:43:18 +0300

    random test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,19 @@
 import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "@/utils/trpc";
 
-export default function Home() {
-  const hello = trpc.useQuery(['hello', { text: 'client' }]);
-  if (!hello.data) {
-    return <div>Loading...</div>;
-  }
+export default function Home(){
+
+  const [first, second] = getOptionsForVote();
+
   return (
-    <div>
-      <p>{hello.data.greeting}</p>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex items-center justify-between max-w-2xl ">
+        <div className="w-16 h-16 bg-red-800">{first}</div>
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-800">{second}</div>
+      </div>
     </div>
   );
-};
-
-// export default function Home(){
-
-//   const [first, second] = getOptionsForVote();
-
-//   return (
-//     <div className="h-screen w-screen flex flex-col justify-center items-center">
-//       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
-//       <div className="p-2" />
-//       <div className="border rounded p-8 flex items-center justify-between max-w-2xl ">
-//         <div className="w-16 h-16 bg-red-800">{first}</div>
-//         <div className="p-8">Vs</div>
-//         <div className="w-16 h-16 bg-red-800">{second}</div>
-//       </div>
-//     </div>
-//   );
-// }
+}
