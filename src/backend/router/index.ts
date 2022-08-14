b8100b40576ec412eb8fdc446eb620446cbcb495
Theal Altake <zufa.altake@gmail.com>
2022-08-14 03:34:08 +0300

    Send back way less data

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,19 +1,16 @@
-import * as trpc from '@trpc/server';
-import { z } from 'zod';
-import { PokemonClient } from 'pokenode-ts';
+import * as trpc from "@trpc/server";
+import { z } from "zod";
+import { PokemonClient } from "pokenode-ts";
 
+export const appRouter = trpc.router().query("get-pokemon-by-id", {
+  input: z.object({ id: z.number() }),
+  async resolve({ input }) {
+    const api = new PokemonClient();
 
-export const appRouter = trpc
-  .router().query("get-pokemon-by-id", {
-    input: z.object( { id: z.number()}),
-    async resolve({input}) {
-      const api = new PokemonClient();
-
-      const pokemon = await api.getPokemonById(input.id);
-      return pokemon; 
-    }
-  });
-
+    const pokemon = await api.getPokemonById(input.id);
+    return { name: pokemon.name, sprites: pokemon.sprites };
+  },
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
