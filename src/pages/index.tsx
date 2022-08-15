4045ce92a0701e50ce416bab1a5c940e6e7ddad4
Theal Altake <zufa.altake@gmail.com>
2022-08-15 16:39:39 +0300

    added buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ import { useState } from "react";
 
 //https://youtu.be/PKy2lYEnhgs?t=3024
 
+const btn =
+  "inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+
 export default function Home() {
   const [ids, updateIds] = useState(() => getOptionsForVote());
 
@@ -25,7 +28,7 @@ export default function Home() {
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex items-center justify-between max-w-2xl ">
-        <div className="w-64 h-64 flex flex-col">
+        <div className="w-64 h-64 flex flex-col items-center">
           <img
             src={firstPokemon.data?.sprites.front_default}
             className="w-full"
@@ -33,9 +36,12 @@ export default function Home() {
           <div className="text-xl text-center capitalize mt-[-2rem]">
             {firstPokemon.data?.name}
           </div>
+          <button className={btn} onClick={() => voteForRoundest(first)}>
+            Rounder
+          </button>
         </div>
         <div className="p-8">Vs</div>
-        <div className="w-64 h-64 flex flex-col">
+        <div className="w-64 h-64 flex flex-col items-center">
           <img
             src={secondPokemon.data?.sprites.front_default}
             className="w-full"
@@ -43,6 +49,9 @@ export default function Home() {
           <div className="text-xl text-center capitalize mt-[-2rem]">
             {secondPokemon.data?.name}
           </div>
+          <button className={btn} onClick={() => voteForRoundest(second)}>
+            Rounder
+          </button>
         </div>
         <div className="p-2" />
       </div>
